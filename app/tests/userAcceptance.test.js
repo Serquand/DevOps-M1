a8bb115a1c38771f2bf141c4b56fcc4ddb45c6db
Serquand <esteban.vincent@efrei.net>
2024-06-29 22:10:35 +0200

    Update userAcceptance test

--- app/tests/userAcceptance.test.js
+++ app/tests/userAcceptance.test.js
@@ -1,43 +1,28 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const puppeteer = require('puppeteer')
 
-let browser;
-
-async function canReachWebsite() {
-    browser = await puppeteer.launch({ headless: true });
-    const page = await browser.newPage();
-    try {
-        await page.goto(`http://${process.env.VPS_IP}:3000`, { waitUntil: 'networkidle2' });
-        return { status: true, content: page }
-    } catch {
-        await browser.close();
-        return { status: false, content: null }
-    }
-}
+const { JSDOM } = require('jsdom');
 
 describe('User Acceptance Test', () => {
-    let reachableWebsite;
-    let isWebsiteAccessible;
+    let website;
 
-    beforeAll(async () => reachableWebsite = await canReachWebsite());
-    afterAll(async () => await browser.close());
-
-    it('should access website', () => {
-        isWebsiteAccessible = reachableWebsite.status;
-        expect(isWebsiteAccessible).toBe(true);
+    beforeAll(async () => {
+        website = await fetch(`http://${process.env.VPS_IP}:3000`);
     });
 
-    it('should see title', async () => {
-        expect(isWebsiteAccessible).toBe(true);
+    it('Is the website reachable', () => expect(website.status).toBe(200))
 
-        const page = reachableWebsite.content;
-        await page.waitForSelector('h1');
-        const h1Text = await page.evaluate(() => {
-            const h1Element = document.querySelector('h1');
-            return h1Element ? h1Element.textContent : null;
-        });
+    it('Does the website have the main titles', async () => {
+        const htmlContent = await website.text();
+        const dom = new JSDOM(htmlContent);
+        const document = dom.window.document;
 
-        expect(h1Text).toBe("DevOps M1 | Project");
+        const mainTitle = document.querySelector('h1').textContent
+        const toDoTitle = document.querySelector('#todo-container h2').textContent
+        const doneTitle = document.querySelector('#done-container h2').textContent
+
+        expect(mainTitle).toBe("DevOps M1 | Project")
+        expect(toDoTitle).toBe("To Do List")
+        expect(doneTitle).toBe("Done List")
     })
 });
